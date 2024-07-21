699548da015dfd54fc993558439878e815cc5368
ch.jai teja <chongalateja1234@gmail.com>
2024-07-21 22:45:13 +0530

    fixed minior bugs at backend

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -7,18 +7,16 @@ exports.verifyJwt = asyncHandler(async (request, _, next) => {
     try {
         const token =
             request.cookies?.accessToken ||
-            request.headers("Authorization")?.replace("Bearer ", "");
-
+            request.headers["authorization"]?.replace("Bearer ", "");
         if (!token) {
             throw new ApiError(401, "Unauthorized request");
         }
-        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
         // optimize the selection of field
         const user = await User.findById(decodedToken?._id).select(
             "-password -refreshToken -projects -collaborations",
         );
-
         if (!user) {
             throw new ApiError(401, "Invalid Access Token ");
         }
